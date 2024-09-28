ef22c43d3ef5cf2f311e5638f02615ac4c413a17
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2024-09-29 01:38:22 +0600

    string

---
+++ JS basic/First_Basic/5_string.js
@@ -0,0 +1,29 @@
+const name = "Nayeem"
+const rate = 50
+
+// console.log(name + rate + " = very low");
+// ata back dateated
+
+console.log(`hello girls my name is ${name} and rate each night ${rate} taka`);
+
+const name2 = new String('Maruf')
+console.log(name2[0]);
+console.log(name2.length);
+
+console.log(name2.toUpperCase());//original value change hoy nay Maruf e ache
+console.log(name2.charAt(1));//kono index a kon char acha ota dakay
+console.log(name2.indexOf('r'))//r kon index a ache ota bolbe
+
+
+const news = name2.substring(1,2)// last number include hobe nah
+
+console.log(news);
+
+const s = "   maruf    "
+console.log(s.trim());//trim space remove kore
+
+
+const s2 = "sdfsd1 sdsdsd 1 wdsd 1 11 1 ewew ew1 1 1 1 "
+console.log(s2.replace('1','-'));
+
+
--- JS basic/First_Basic/dataType.js
+++ JS basic/First_Basic/dataType.js
@@ -8,20 +8,16 @@ const v1 = 2323.23
 const id = Symbol("123");
 const id2 = Symbol("123");
 // value same pass korsi kintu Symbol change kore disse 
-console.log(id == id2);//-> false
+// console.log(id == id2);//-> false
 const bignumber = 34343434343434343434
-console.log(bignumber);
-
-
-
-
+// console.log(bignumber);
 
 // Reference (Non primitive)
 
 // Array, Objects, Functions
 
 const a = ["fsdf","fasdf","asdf",456]
-console.log(a);
+// console.log(a);
 
 
 //object
@@ -36,3 +32,23 @@ const myFunction = function(){
     console.log("hello girls");
     
 }
+
+
+// +++++++++++++++++++++++++++++++++++++++
+
+// stack(primitive) , heap(non primitive)
+// stack a kono value nile like a = b hoyle ar copy hoy
+// b = 5 korle a change hobe nah
+// but heap a kono value ayle like a=b hoyle ,b change korle a change hobe
+let user1={
+    email:"asc",
+    phone:12344,
+}
+let user2 = user1
+user2.email= "ahammedmaruf007@gmail.com"
+console.log(user1);
+console.log(user2);
+/// 2 tay change hoya jabe karon ata heap a ache
+
+
+
