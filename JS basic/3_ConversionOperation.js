89a7b338f9ef6cc2b0f0fbec83142cd02a03faf8
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2024-09-28 22:45:00 +0600

    Operation

--- JS basic/3_ConversionOperation.js
+++ JS basic/3_ConversionOperation.js
@@ -8,8 +8,8 @@ let age = null
 //console.log(typeof age)
 
 let valueinNnumber = Number(age)
-console.log(typeof valueinNnumber)
-console.log(valueinNnumber);//strign ar vitor number sara char thakle "NaN" print hobe
+// console.log(typeof valueinNnumber)
+// console.log(valueinNnumber);//strign ar vitor number sara char thakle "NaN" print hobe
 // null hoyle 0 print korbe
 // bool korle 0/1 asbe
 // "3"=3
@@ -18,7 +18,36 @@ console.log(valueinNnumber);//strign ar vitor number sara char thakle "NaN" prin
 
  let islog = "dfgdf"
  let boolislog = Boolean(islog)
- console.log(boolislog)
+//  console.log(boolislog)
  // ""->false
  // "sdfs" true
+
+
+ // string a convert kora jabe same vabe
+
+
+ ////////////////////////OPeration//////////////////////
+
+let n =5;
+let negativen = -n;
+// console.log(n);
+n+=n
+// console.log(n);
+n%=3
+// console.log(n);
+
+let s1 = "hello",s2="girls"
+let s3 = s1+ " " + s2
+console.log(s1+" "+s2);
+console.log(s3);
+
+console.log("2"+2+2);
+console.log(2+2+"2");
+
+
+
+
+
+
+
  
