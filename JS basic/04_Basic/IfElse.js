06f4ed32cb642bece202ac258bfed9fb923b73f5
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2024-10-06 22:30:25 +0600

    If Else

---
+++ JS basic/04_Basic/IfElse.js
@@ -0,0 +1,72 @@
+///if
+
+//java execution context
+//function execution context
+
+//sob code global execution thake run hobe
+
+
+//04_control_flow
+//control flow hosse jodi ami login thaki tahle only login ar code run hobe
+//or ami jodi log out thaki tahole only log out ar code run hobe
+//sob code run hobe nah atay control flow
+
+// if
+const isUserloggedIn = true
+const temperature = 41
+
+// if ( temperature === 40 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("Execute");
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2");
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 750");
+    
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
+
+
+
+
+
+
+
