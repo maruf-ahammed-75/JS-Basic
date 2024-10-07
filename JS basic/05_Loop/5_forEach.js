f0a98b452e150bf762fb464d9be1df4f8a81bd2b
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2024-10-07 23:22:26 +0600

    forEach

---
+++ JS basic/05_Loop/5_forEach.js
@@ -0,0 +1,28 @@
+
+// forEach
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// coding.forEach(function (i){
+//     console.log(i);
+    
+// })
+
+
+const myCoding = [//ata array ar vitor object
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myCoding.forEach((item) => {
+    console.log(item);
+})
