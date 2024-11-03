dd0543d480c44019ca4b5005750476910f877cf0
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2024-11-03 21:52:04 +0600

    All

--- Js Backend/Backend/server.js
+++ Js Backend/Backend/server.js
@@ -5,4 +5,24 @@ app.get('/',(req,res)=>{
     res.send('server is ready');
 });
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
+
+app.get('jokes',(req,res)=>{
+    const jokes = [
+        {
+            id:1,
+            title:"a joke",
+            content:"First joke"
+        },
+        {
+            id:2,
+            title:"a joke",
+            content:"second joke"
+        }
+    ];
+    res.send(jokes);
+})
+
+app.listen(port,()=>{
+    console.log(`server at ${port}`);
+});
