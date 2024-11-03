2f8e9388d3bd17d8472a106961b389c3d2a2f414
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2024-11-03 21:59:04 +0600

    JSX

--- Js Backend/Frontend/src/App.jsx
+++ Js Backend/Frontend/src/App.jsx
@@ -7,7 +7,7 @@ import axios from 'axios'
 function App() {
   const [jokes,setJokes] = useState([])
   useEffect(()=>{
-    axios.get('http://localhost:3001/jokes')
+    axios.get('/api/jokes')
     .then((Response)=>{
       setJokes(Response.data)
     })
